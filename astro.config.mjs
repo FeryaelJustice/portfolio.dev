9c21ef7197a7b113d1ee4a1526abac29f5f24f81
FeryaelJustice <nano9gs@hotmail.es>
2025-09-15 23:24:18 +0200

    upgrade to tailwind 4 and add vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   i18n: {
     defaultLocale: "en",
