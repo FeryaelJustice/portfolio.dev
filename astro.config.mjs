659f86b66429330e857fce80a53648af72651a74
FeryaelJustice <nano9gs@hotmail.es>
2026-02-16 13:38:02 +0100

    update proyectos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+    integrations: [react()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  i18n: {
-    defaultLocale: "en",
-    locales: ["es", "en",],
-  },
+    i18n: {
+        defaultLocale: "en",
+        locales: ["es", "en"],
+    },
 });
